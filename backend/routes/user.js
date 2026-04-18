fa5b940e3deb3cad9d35dee6b78c5c95c01454f8
sumit saraswat <dimmunokha@gmail.com>
2026-04-18 16:51:00 +0530

    Update generative model from 'gemini-2.5-flash-lite' to 'gemini-2.5-flash'

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -210,7 +210,7 @@ router.post("/genratequiz", middleware,async (req, res) => {
         }
 
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
         const { topic, numques, difficulty } = req.body;
 
         if (!topic || !numques || !difficulty) {
